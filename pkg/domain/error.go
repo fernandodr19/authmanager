fb29a1067e2fcfcd93c4c75e76c1e5dcf03c1875
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-03 16:51:08 -0300

    chore: remove params

--- pkg/domain/error.go
+++ pkg/domain/error.go
@@ -1,29 +1,23 @@
 package domain
 
 import (
-	"encoding/json"
 	"fmt"
 )
 
-type Params map[string]interface{}
-
 type BasicDomainError struct {
-	op     string  // domain operations stack for pretty error stacktrace
-	err    error   // the error itself
-	params *Params // params sent to the last called function which triggered the error (optional)
+	op  string // domain operations stack for pretty error stacktrace
+	err error  // the error itself
 
 	parent *BasicDomainError
 }
 
-func Error(op string, err error, opts ...Params) *BasicDomainError {
+// Error stack up errors for better tracing
+func Error(op string, err error) *BasicDomainError {
 	if err == nil {
 		return nil
 	}
 
 	domainError := BasicDomainError{op: op, err: err}
-	if len(opts) > 0 {
-		domainError.params = &opts[0]
-	}
 	if derr, ok := err.(*BasicDomainError); ok {
 		derr.parent = &domainError
 	}
@@ -31,26 +25,14 @@ func Error(op string, err error, opts ...Params) *BasicDomainError {
 	return &domainError
 }
 
+// Unwrap returns the err itself
 func (e BasicDomainError) Unwrap() error {
 	return e.err
 }
 
+// Error returns a string formatted error
 func (e BasicDomainError) Error() string {
-	return fmt.Sprintf("%s%s%s%s", e.op, e.calledWithParams(), e.separator(), e.err.Error())
-}
-
-func (e BasicDomainError) calledWithParams() string {
-	if !e.hasParentErr() || e.parent.params == nil {
-		return "()"
-	}
-
-	keyValues, _ := json.Marshal(e.parent.params)
-	keyValues = keyValues[1 : len(keyValues)-1] // drop json braces
-	return fmt.Sprintf("(%s)", keyValues)
-}
-
-func (e BasicDomainError) hasParentErr() bool {
-	return e.parent != nil
+	return fmt.Sprintf("%s%s%s", e.op, e.separator(), e.err.Error())
 }
 
 func (e BasicDomainError) separator() string {
