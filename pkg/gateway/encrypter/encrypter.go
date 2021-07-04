9dd6d4cbf316a9f365670de5f59973e82428a95d
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-04 08:11:09 -0300

    docs: doc some methods

--- pkg/gateway/encrypter/encrypter.go
+++ pkg/gateway/encrypter/encrypter.go
@@ -8,9 +8,11 @@ import (
 
 var _ accounts.Encrypter = &Encrypter{}
 
+// Encrypter handles password encryption & validation
 type Encrypter struct {
 }
 
+// HashedPassword hashes a password
 func (c Encrypter) HashedPassword(password vos.Password) (vos.HashedPassword, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -20,6 +22,7 @@ func (c Encrypter) HashedPassword(password vos.Password) (vos.HashedPassword, er
 	return vos.HashedPassword(hash), nil
 }
 
+// PasswordMathces validate if plain password matchs hashed password
 func (c Encrypter) PasswordMathces(password vos.Password, hashedPassword vos.HashedPassword) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
