574c963926d468b4b7a05979adc55b1bce33a653
Fernando Da Rós <fernando.ros@stone.com.br>
2021-07-04 07:48:04 -0300

    docs: doc some methods

--- pkg/gateway/authorizer/errors.go
+++ pkg/gateway/authorizer/errors.go
@@ -3,6 +3,8 @@ package authorizer
 import "errors"
 
 var (
+	// ErrInvalidToken invalid token error
 	ErrInvalidToken = errors.New("token is invalid")
+	// ErrInvalidToken expired token error
 	ErrExpiredToken = errors.New("token has expired")
 )
